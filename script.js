223cd8923b39217061c6d863a92f8ac68b04f74a
Navyaarekuti <arekutinavya1@gmail.com>
2023-07-11 11:08:04 -0500

    done again

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ window.addEventListener("load", function() {
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
-   console.log(listedPlanetsResponse);
+   //console.log(listedPlanetsResponse);
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
-       console.log(listedPlanets);
+      // console.log(listedPlanets);
    }).then(function () {
-       console.log(listedPlanets);
+      // console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       let planetPicked = pickPlanet(listedPlanets);
-       console.log(planetPicked);
+       let planetSelected = pickPlanet(listedPlanets);
+       //console.log(planetSelected);
        
-        console.log(planetPicked.name);
+       // console.log(planetSelected.name);
 
-       addDestinationInfo(document, planetPicked.name, planetPicked.diameter, planetPicked.star, planetPicked.distance, planetPicked.moons, planetPicked.image);
+       addDestinationInfo(document, planetSelected.name, planetSelected.diameter, planetSelected.star, planetSelected.distance, planetSelected.moons, planetSelected.image);
     })
    
 });
